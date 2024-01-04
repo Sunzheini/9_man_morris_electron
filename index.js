d5e553b85dad36f8ccfa332461cd5d25a2ce3089
Sunzheini <daniel_zorov@abv.bg>
2024-01-04 15:17:04 +0200

    logic improvement

--- index.js
+++ index.js
@@ -4,20 +4,83 @@ let infoContainer = document.getElementsByClassName('info-container')[0];
 let player1 = true;
 let player1Collection = {};
 let player2Collection = {};
+const Piece3Combinations = [
+    ['button-row1-col1', 'button-row1-col4', 'button-row1-col7'],
+    ['button-row2-col2', 'button-row2-col4', 'button-row2-col6'],
+    ['button-row3-col3', 'button-row3-col4', 'button-row3-col5'],
+    ['button-row4-col1', 'button-row4-col2', 'button-row4-col3'],
+    ['button-row4-col5', 'button-row4-col6', 'button-row4-col7'],
+    ['button-row5-col3', 'button-row5-col4', 'button-row5-col5'],
+    ['button-row6-col2', 'button-row6-col4', 'button-row6-col6'],
+    ['button-row7-col1', 'button-row7-col4', 'button-row7-col7'],
+
+    ['button-row1-col1', 'button-row4-col1', 'button-row7-col1'],
+    ['button-row2-col2', 'button-row4-col2', 'button-row6-col2'],
+    ['button-row3-col3', 'button-row4-col3', 'button-row5-col3'],
+    ['button-row1-col4', 'button-row2-col4', 'button-row3-col4'],
+    ['button-row5-col4', 'button-row6-col4', 'button-row7-col4'],
+    ['button-row3-col5', 'button-row4-col5', 'button-row5-col5'],
+    ['button-row2-col6', 'button-row4-col6', 'button-row6-col6'],
+    ['button-row1-col7', 'button-row4-col7', 'button-row7-col7'],
+];
+let currentlyRemovingAPiece = false;
+let player1MoveSequence = [];
+let player2MoveSequence = [];
 
 initializePlayerCollections();
-console.log(player1Collection);
 
 
 for (let i = 0; i < buttonsList.length; i++) {
     buttonsList[i].addEventListener('click', (event) => {
-        try{
-            colorTheButton.call(buttonsList[i]);
-            logicController(buttonsList[i], player1);
-            player1 = !player1;
-            changeInfoContainer();
-        } catch (e) {
-            infoContainer.innerHTML = e;
+        if (currentlyRemovingAPiece) {
+            if(player1) {
+                if(player2Collection[buttonsList[i].classList[1]].owned) {
+                    // remove the piece
+                    buttonsList[i].classList.remove('owned-by-player2');
+                    player2Collection[buttonsList[i].classList[1]].owned = false;
+
+                    // prepare for next turn
+                    player1 = !player1;
+                    changeInfoContainer();
+                    currentlyRemovingAPiece = false;
+                } else {
+                    infoContainer.innerHTML = "Not owned by player 2";
+                }
+            } else {
+                if(player1Collection[buttonsList[i].classList[1]].owned) {
+                    // remove the piece
+                    buttonsList[i].classList.remove('owned-by-player1');
+                    player1Collection[buttonsList[i].classList[1]].owned = false;
+
+                    // prepare for next turn
+                    player1 = !player1;
+                    changeInfoContainer();
+                    currentlyRemovingAPiece = false;
+                } else {
+                    infoContainer.innerHTML = "Not owned by player 1";
+                }
+            }
+        } else {
+            try {
+                if(player1Collection[buttonsList[i].classList[1]].owned || player2Collection[buttonsList[i].classList[1]].owned) {
+                    infoContainer.innerHTML = "Already owned by a player";
+                } else {
+                    // do actions
+                    colorTheButton.call(buttonsList[i]);
+                    addButtonToOwned(buttonsList[i], player1);
+
+                    if(checkFor3Pieces(player1)) {
+                        currentlyRemovingAPiece = true;
+                        return;
+                    }
+
+                    // prepare for next turn
+                    player1 = !player1;
+                    changeInfoContainer();
+                }
+            } catch (e) {
+                infoContainer.innerHTML = e;
+            }
         }
     });
 }
@@ -72,23 +135,71 @@ function initializePlayerCollections() {
 }
 
 
-function logicController(button, player1) {
-    if(!checkIfNotAlreadyOwned(button, player1)){
+function addButtonToOwned(button, player1) {
+    if (player1) {
+        player1Collection[button.classList[1]].owned = true;
+        player1Collection[button.classList[1]].row = button.classList[1][10];
+        player1Collection[button.classList[1]].col = button.classList[1][15];
+    } else {
+        player2Collection[button.classList[1]].owned = true;
+        player2Collection[button.classList[1]].row = button.classList[1][10];
+        player2Collection[button.classList[1]].col = button.classList[1][15];
+    }
+}
 
+
+// ToDo: check for 3 pieces only against the latest button clicked!
+function checkFor3Pieces(player1) {
+    // check for 3 in a row
+    if(player1) {
+        // check the keys in player1Collection
+        for (let key in player1Collection) {
+
+            // check if the current key is owned
+            if (player1Collection[key].owned) {
+                // check if the current key is in the Piece3Combinations
+                for (let i = 0; i < Piece3Combinations.length; i++) {
+                    if (Piece3Combinations[i].includes(key)) {
+                        // check if all 3 keys are owned
+                        if (player1Collection[Piece3Combinations[i][0]].owned &&
+                            player1Collection[Piece3Combinations[i][1]].owned &&
+                            player1Collection[Piece3Combinations[i][2]].owned) {
+
+                            // now check the player1WinSequence
+                            if(!player1MoveSequence.includes(Piece3Combinations[i])){
+                                player1MoveSequence.push(Piece3Combinations[i]);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        // check the keys in player2Collection
+        for (let key in player2Collection) {
+
+            // check if the current key is owned
+            if (player2Collection[key].owned) {
+                // check if the current key is in the Piece3Combinations
+                for (let i = 0; i < Piece3Combinations.length; i++) {
+                    if (Piece3Combinations[i].includes(key)) {
+                        // check if all 3 keys are owned
+                        if (player2Collection[Piece3Combinations[i][0]].owned &&
+                            player2Collection[Piece3Combinations[i][1]].owned &&
+                            player2Collection[Piece3Combinations[i][2]].owned) {
+
+                            // now check the player2WinSequence
+                            if(!player2MoveSequence.includes(Piece3Combinations[i])){
+                                player2MoveSequence.push(Piece3Combinations[i]);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
-
-
-    checkFor3Pieces();
+    return false;
 }
-
-function checkIfNotAlreadyOwned(button, player1) {
-
-
-}
-
-function checkFor3Pieces() {
-    // check for 3 in a row
-    // check for 3 in a column
-}
-
